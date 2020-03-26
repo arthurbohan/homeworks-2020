7afbe7256b250674de1e9fc636c160b331e3834e
Arthur <arthurbokhan@gmail.com>
2020-03-26 20:59:16 +0300

    changed select to find

--- HW02/Arthur_Bohan/arthur_bohan_hw_02_t_01.rb
+++ HW02/Arthur_Bohan/arthur_bohan_hw_02_t_01.rb
@@ -5,7 +5,7 @@ lines = File.readlines('input_hw_02_t_01.txt', chomp: true)
 
 # select line with error
 def find_errors(lines)
-  lines.select { |line| line.downcase.include? 'error' }
+  lines.find { |line| line.downcase.include? 'error' }
 end
 
 puts find_errors(lines)
