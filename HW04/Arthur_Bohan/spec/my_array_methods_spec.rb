ccb1ed4994a67c7ffd36d5183de8c58cec331132
Arthur <arthurbokhan@gmail.com>
2020-04-13 12:01:13 +0300

    rework for new check

--- HW04/Arthur_Bohan/spec/my_array_methods_spec.rb
+++ HW04/Arthur_Bohan/spec/my_array_methods_spec.rb
@@ -1,49 +1,61 @@
-require_relative '../methods.rb'
+require_relative '../my_array_methods.rb'
 
-RSpec.describe Array do
+describe Array do
   let(:array) { [1, 2, 3] }
 
   describe '#my_select' do
-    context 'when method works correct' do
+    context 'when select spesific number from array' do
       let(:expected_result) { [1] }
 
-      it 'should return a specific element from array' do
+      it 'returns a specific element from array' do
         expect(array.my_select { |elem| elem == 1 }).to eq expected_result
       end
     end
 
-    context 'boundary value analysis' do
+    context 'when select positive number from array' do
       let(:expected_result) { [1, 2, 3] }
 
-      it 'should return full number from array' do
+      it 'returns full number from array' do
         expect(array.my_select(&:positive?)).to eq expected_result
       end
     end
 
-    context 'if we select an element outside bounds' do
+    context 'when select an element outside bounds' do
       let(:expected_result) { [] }
 
-      it 'should return clear array' do
+      it 'returns clear array when select 0' do
         expect(array.my_select(&:zero?)).to eq expected_result
+      end
+    end
+
+    context 'when select negative numbers' do
+      let(:expected_result) { [] }
+
+      it 'returns clear array' do
         expect(array.my_select(&:negative?)).to eq expected_result
       end
     end
   end
 
   describe '#my_map' do
-    context 'when method works correct' do
-      it 'should do somthing with all elements from array' do
+    context 'when multiply elements' do
+      it 'does somthing with all elements from array' do
         expect(array.my_map { |elem| elem * 2 }).to eq [2, 4, 6]
+      end
+    end
+
+    context 'when exponentiate elements' do
+      it 'does somthing with all elements from array' do
         expect(array.my_map { |elem| elem**2 }).to eq [1, 4, 9]
       end
     end
   end
 
   describe '#my_each' do
-    context 'wnen method works correct' do
+    context 'when method works correct' do
       let(:new_array) { [] }
 
-      it 'should sort out elements and passed them to the block' do
+      it 'sorts out elements and passed them to the block' do
         array.my_each { |elem| new_array << (elem - 1) }
         expect(new_array).to eq [0, 1, 2]
       end
